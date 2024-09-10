22647c808a0c171984aaa53e1e19e39f341f37df
Deepak kumar <149260467+deepak40021@users.noreply.github.com>
2024-09-10 09:28:16 +0000

    new changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountid = 122112
+
+let email = "deepakforwork@gmail.com"
+
+var password = "332321"
+
+city = " kolkata"
+
+console.table([accountid, email , password , city])
+
+/*              
+
+prefer not to use  let keyword for assinging the variable....always use let and for constant it is fixxed to use const
+
+
+
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict " ;    // treats all js code as newer version
+
+//  we are using node js not browser , so it will  not run here alert ( 3+3 )
+
+    
+
+
+
+/* 
+ data types in js :-
+
+ number = 2 to the power 52
+ bigint
+ string = " "
+ null = stand alone value 
+ boolean = true/false
+ undefined 
+ symbol = unique
+
+
+ object 
+
+
+
+*/
+ console.log(typeof null)
+
+
+
+ // "33"=33
+ //"33abc"= NaN ( not an number )
