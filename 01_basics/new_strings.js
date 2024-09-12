62a9f5ec78f2f307676d8a5097f6f25854dbd312
Deepak kumar <149260467+deepak40021@users.noreply.github.com>
2024-09-12 12:54:33 +0000

    new

---
+++ 01_basics/new_strings.js
@@ -0,0 +1,7 @@
+const name = "deepak kumar"
+const age = "19"
+
+console.log(name+ " " +age)
+console.log(`hello my name is  ${name} and my age is ${age}`)
+console.log(name[2])
+console.log(name.__proto__)
---
+++ 01_basics/numsmath.js
@@ -0,0 +1,16 @@
+//const score =12 
+//console.log(score);
+
+
+//const balance = new Number(1000)
+
+//console.log(balance);
+
+//console.log(balance.toString().length)
+
+
+
+//console.log(Math)
+
+
+console.log(Math.random())
