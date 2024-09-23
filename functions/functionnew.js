727af4bcfc70c93dfc47283b05c8d5b01da36dcf
Deepak kumar <149260467+deepak40021@users.noreply.github.com>
2024-09-23 08:39:19 +0000

    .

---
+++ functions/functionnew.js
@@ -0,0 +1,33 @@
+ /*function myname(){
+  console.log("D")
+  console.log("r")
+  console.log("r")
+  console.log("p")
+  console.log("a")
+  console.log("k")
+
+
+}
+
+myname()
+
+function  justloggedin(username){
+ console.log ( `${username} just logged in` )
+}
+
+
+justloggedin("deepak")
+*/
+const user = {
+  usernamr : "deepak",
+  price: 1200
+}
+
+
+function handleobject(user){
+
+  console.log(`the nameof the product  is ${user.usernamr} and the price of the product is ${user.price}`)
+}
+
+
+handleobject(user)
